805b1550bac23ca5afb541b7734b836bebb7f6c4
Petrik Lajos Szg <info@petrik.hu>
2019-03-04 16:28:36 +0100

    Initial commit

---
+++ UnitTest/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class JaratKezelo
+    {
+        public List<Jarat> lista = new List<Jarat>();
+        public void UjJarat(String JaratSzam, String HonnanRepter, String Hovarepter, DateTime Indulas)
+        {
+            Jarat j = new Jarat(JaratSzam, HonnanRepter, Hovarepter, Indulas);
+            lista.Add(j);
+        }
+        public void Keses(string jaratSzam, int keses)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].JaratSzam.Equals(jaratSzam))
+                {
+                    lista[i].Keses = keses;
+                }
+            }
+        }
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            return DateTime.Now;
+        }
+        public List<string> JaratokRepuloterrol(string repter)
+        {
+            List<string> vissza = new List<string>();
+            return vissza;
+        }
+    }
+
+
+
+
+
+    public class Jarat
+    {
+        public Jarat(string jaratSzam, string honnanRepter, string hovarepter, DateTime indulas)
+        {
+            JaratSzam = jaratSzam;
+            HonnanRepter = honnanRepter;
+            Hovarepter = hovarepter;
+            Indulas = indulas;
+        }
+
+        public String JaratSzam { get; set; }
+        public String HonnanRepter { get; set; }
+        public String Hovarepter { get; set; }
+        public DateTime Indulas { get; set; }
+        public int Keses { get; set; }
+    }
+    class NegatvKesesExcepton : Exception
+    {
+
+
+        public NegatvKesesExcepton(string name)
+            : base(String.Format("Invalid Student Name: {0}", name))
+        {
+
+        }
+
+    }
+}
---
+++ UnitTestTests/JaratKezeloTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Tests
+{
+    [TestClass()]
+    public class JaratKezeloTests
+    {
+        [TestMethod()]
+        public void UjJaratTest()
+        {
+            JaratKezelo j = new JaratKezelo();
+            j.UjJarat("12345", "Budapest", "Berlin", new DateTime(2008, 3, 9, 16, 5, 7, 123));
+            j.Keses("12345", 5);
+            Assert.AreEqual(j.lista[0].Keses,5);
+        }
+
+        [TestMethod()]
+        public void KesesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MikorIndulTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void JaratokRepuloterrolTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
